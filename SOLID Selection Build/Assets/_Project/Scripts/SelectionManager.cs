0e6be2c730df4be54dee3cb48f1e3247b02347b6
khasselmeier <123027444+khasselmeier@users.noreply.github.com>
2024-11-14 16:01:42 -0700

    added Charles Amat's SOLID tutorial

---
+++ SOLID Selection Build/Assets/_Project/Scripts/SelectionManager.cs
@@ -0,0 +1,30 @@
+﻿﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionManager : MonoBehaviour
+{
+    private IRayProvider _rayProvider;
+    private ISelector _selector;
+    private ISelectionResponse _currentSelectionResponse;
+
+    private Transform _currentSelection;
+
+    private void Awake()
+    {
+        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+
+        _rayProvider = GetComponent<IRayProvider>();
+        _selector = GetComponent<ISelector>();
+        _currentSelectionResponse = GetComponent<ISelectionResponse>();
+    }
+
+    private void Update()
+    {
+        if (_currentSelection != null) _currentSelectionResponse.OnDeselect(_currentSelection);
+        _selector.Check(_rayProvider.CreateRay());
+        _currentSelection = _selector.GetSelection();
+
+        if (_currentSelection != null) _currentSelectionResponse.OnSelect(_currentSelection);
+    }
+}
